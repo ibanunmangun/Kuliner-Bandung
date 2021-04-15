9b1caeedaf9daa428598102fb18d50f5658cb19a
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-04-15 22:39:19 +0700

    Add files via upload

---
+++ data/PointofInterestBandung_2.js
@@ -0,0 +1,22 @@
+var json_PointofInterestBandung_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterestBandung_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Bubur Ayam Mang H Oyo Pusat (ASLI)", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.61588, -6.90496 ] } },
+{ "type": "Feature", "properties": { "Name": "BMC (Bandoengsche Melk Centrale", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.608158939108591, -6.910786601976064 ] } },
+{ "type": "Feature", "properties": { "Name": "Taman Sejarah Bandung", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.609817340762888, -6.910269874586348 ] } },
+{ "type": "Feature", "properties": { "Name": "Kartika Sari - Kebon Jukut", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.605302275137944, -6.914409889476118 ] } },
+{ "type": "Feature", "properties": { "Name": "Warung Sate HM Harris", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.617133308410089, -6.922180626792407 ] } },
+{ "type": "Feature", "properties": { "Name": "Jalan Asia Afrika", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6098845, -6.9214801 ] } },
+{ "type": "Feature", "properties": { "Name": "Cuanki Cikapundung", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.608244440762888, -6.920137674218849 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Seni Ropih", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.609591401608469, -6.918778734880595 ] } },
+{ "type": "Feature", "properties": { "Name": "Braga Permai", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.60934, -6.91746 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Raya Bandung", "X": 107.606097, "Y": -6.921605, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.606097, -6.921605 ] } },
+{ "type": "Feature", "properties": { "Name": "Braga Citywalk", "X": 107.608871, "Y": -6.917123, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.608871, -6.917123 ] } },
+{ "type": "Feature", "properties": { "Name": "Polrestabes Bandung", "X": 107.610479, "Y": -6.913962, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.610479, -6.913962 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Pertamina 34-40127", "X": 107.61193, "Y": -6.919915, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.61193, -6.919915 ] } },
+{ "type": "Feature", "properties": { "Name": "Apotek Kimia Farma Braga", "X": 107.609943, "Y": -6.921145, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.609943, -6.921145 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Sakit Umum Bungsu", "X": 107.613302, "Y": -6.918556, "Keterangan": null }, "geometry": { "type": "Point", "coordinates": [ 107.613302, -6.918556 ] } }
+]
+}
